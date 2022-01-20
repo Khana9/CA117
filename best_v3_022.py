edbf0c831038d2f08488c30f6fcde364e9ba6889
Khana9 <abdul.khan9@mail.dcu.ie>
2022-01-20 15:17:42 +0000

    code

---
+++ best_v3_022.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#/usr/bin/env python3
+
+import sys
+
+file = sys.argv[1]
+err = 0
+
+with open(file, "r") as f:
+    f = f.readlines()
+
+    top_mark = 0
+    top_name = 0
+    for line in f:
+        name = 0
+        mark = 0
+        try:
+            i = 0
+            while i < len(line) and line[i] != " ":
+                i = i + 1
+            mark = line[0:i]
+            name = line[i:-1]
+            if int(mark) > int(top_mark):
+                top_mark = int(mark)
+                top_name = name
+        except ValueError:
+            print('Invalid mark', mark, 'encountered. Skipping.')
+
+print('Best student:', top_name.strip())
+print('Best mark:', top_mark)
---
+++ bests_022.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+
+file = sys.argv[1]
+
+with open(file, "r") as f:
+    f = f.readlines()
+
+
+top_mark = 0
+top_name = []
+for line in f:
+    name = 0
+    mark = 0
+    i = 0
+    while i < len(line) and line[i] != " ":
+        i = i + 1
+    mark = line[0:i]
+    name = line[i:-1]
+    if int(mark) > int(top_mark):
+        top_mark = int(mark)
+        top_name = []
+        top_name.append(name)
+    elif int(mark) == int(top_mark):
+        top_name.append(name)
+
+for names in top_name:
+    name = 
+print('Best student(s):', top_name)
+print('Best mark:', top_mark)
